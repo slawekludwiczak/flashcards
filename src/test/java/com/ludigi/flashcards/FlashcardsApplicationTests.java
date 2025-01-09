f75dc748a5d6a0a10f3cf35aec91da20afb6bf47
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2025-01-09 09:59:43 +0100

    init

---
+++ src/test/java/com/ludigi/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludigi.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
